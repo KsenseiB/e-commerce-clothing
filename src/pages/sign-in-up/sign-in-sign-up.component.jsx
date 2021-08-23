2e4481bf77824f35fb37445b06499dace2ce5861
Ksenia <ksu0593@gmail.com>
2021-08-23 16:01:10 +0300

    login form

---
+++ src/pages/sign-in-up/sign-in-sign-up.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import SignIn from '../../components/sign-in/sign-in.component';
+
+import './form-input.styles.scss';
+
+const SignInAndUpPage = () => (
+	<div className='sign-in-and-sign-up'><SignIn /></div>
+)
+
+export default SignInAndUpPage;
