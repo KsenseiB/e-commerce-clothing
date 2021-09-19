6009fef1232c8b2db28425a4493cb869b50bce52
Ksenia <ksu0593@gmail.com>
2021-09-19 14:13:46 +0300

    cartDropdown error

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,5 @@
 import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from 'redux-devtools-extension';
 import logger from 'redux-logger';
 
 import rootReducer from "./root-reducer";
@@ -6,6 +7,6 @@ import rootReducer from "./root-reducer";
 const middlewares = [logger];
 
 //a function that gets both a root reducer and also the return value of applying middleware.
-const store = createStore(rootReducer, applyMiddleware(...middlewares ));
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middlewares )));
 
 export default store;
